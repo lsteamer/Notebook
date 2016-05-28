541181069315004837f606ccb31fb70fbd158881
Antonio Gonzalez <anjegonz@gmail.com>
2016-05-28 14:28:50 +0200

    46 List completed, missing optimization

--- app/src/main/java/com/anjegonz/lsteamer/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/anjegonz/lsteamer/notebook/MainActivityListFragment.java
@@ -8,15 +8,21 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainActivityListFragment extends ListFragment {
 
+    private ArrayList<Note> notes;
+    private NoteAdapter noteAdapter;
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
+
+        /*
         String[] values = new String[]{"Xbox One","PS4","WiiU",
                                         "Xbox 360","PS3","Wii",
                                         "Xbox","PS2","GameCube"};
@@ -24,6 +30,23 @@ public class MainActivityListFragment extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
 
         setListAdapter(adapter);
+        */
+
+        notes = new ArrayList<Note>();
+        notes.add(new Note("This is a new Note title","This is the body of the note",Note.Category.PERSONAL));
+        notes.add(new Note("All of them really","An Original body for every original note",Note.Category.FINANCE));
+        notes.add(new Note("How do you do","I'm doing alright. How about you?",Note.Category.TECHNICAL));
+        notes.add(new Note("Original Title","AS it turns out, this is ",Note.Category.PERSONAL));
+        notes.add(new Note("Wat","All of my wats",Note.Category.QUOTE));
+        notes.add(new Note("Und was?","Ich kann noch kein Deutsch sprechen",Note.Category.QUOTE));
+        notes.add(new Note("Un título","Como el que no tengo en mi casa",Note.Category.PERSONAL));
+        notes.add(new Note("Mis cocos","Me gustan mucho los cocos y a ti?",Note.Category.TECHNICAL));
+        notes.add(new Note("El último","es difícil ser original en el cuerpo de todos y cada uno.",Note.Category.FINANCE));
+
+        noteAdapter = new NoteAdapter(getActivity(),notes);
+
+        setListAdapter(noteAdapter);
+
     }
 
     @Override
--- app/src/main/java/com/anjegonz/lsteamer/notebook/NoteAdapter.java
+++ app/src/main/java/com/anjegonz/lsteamer/notebook/NoteAdapter.java
@@ -1,9 +1,13 @@
 package com.anjegonz.lsteamer.notebook;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 /**
@@ -16,6 +20,27 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
+        // Get the data item for this position
+        Note note = getItem(position);
 
+        // Checks if an existing view is being reused, otherwise, inflate a new view from custom row layout.
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_row, parent, false);
+        }
+
+        //Grab references of views so we can populate them with specific note row data
+        TextView noteTitle = (TextView) convertView.findViewById(R.id.listItemNoteTitle);
+        TextView noteText = (TextView) convertView.findViewById(R.id.listItemNoteBody);
+        ImageView noteIcon = (ImageView) convertView.findViewById(R.id.listItemNoteImg);
+
+        //Fill each new reference view with the data associated with the note its' referencing.
+        noteTitle.setText(note.getTitle());
+        noteText.setText(note.getMessage());
+        noteIcon.setImageResource(note.getAssociatedDrawable());
+
+
+        //now that we modified the view to display the appropriate data, return it so it will be displayed.
+        return convertView;
     }
+
 }
