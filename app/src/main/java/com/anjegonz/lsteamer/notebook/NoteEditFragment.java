dfec6cdcdeb143453c5ee7f644066f9e65ad4e61
Antonio Gonzalez <anjegonz@gmail.com>
2016-05-30 17:44:29 +0200

    66 - Save changes button is implemented graphically. No data saved yet.

--- app/src/main/java/com/anjegonz/lsteamer/notebook/NoteEditFragment.java
+++ app/src/main/java/com/anjegonz/lsteamer/notebook/NoteEditFragment.java
@@ -1,12 +1,16 @@
 package com.anjegonz.lsteamer.notebook;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -16,7 +20,11 @@ import android.widget.ImageButton;
  */
 public class NoteEditFragment extends Fragment {
 
-
+    private ImageButton noteCatButton;
+    private EditText title, message;
+    private Note.Category savedButtonCategory;
+    private AlertDialog categoryDialogObject, confirmDialogObject;
+    private int catcat;
     public NoteEditFragment() {
         // Required empty public constructor
     }
@@ -31,20 +39,111 @@ public class NoteEditFragment extends Fragment {
 
 
         //Grab widget references from layout
-        EditText title = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
-        EditText message = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
-        ImageButton noteCatButton = (ImageButton) fragmentLayout.findViewById(R.id.editNoteButton);
+        title = (EditText) fragmentLayout.findViewById(R.id.editNoteTitle);
+        message = (EditText) fragmentLayout.findViewById(R.id.editNoteMessage);
+        noteCatButton = (ImageButton) fragmentLayout.findViewById(R.id.editNoteButton);
+        Button savedButton = (Button) fragmentLayout.findViewById(R.id.saveNote);
 
         //inflate widget with data
         Intent intent = getActivity().getIntent();
         title.setText(intent.getExtras().getString(MainActivity.NOTE_TITLE_EXTRA));
         message.setText(intent.getExtras().getString(MainActivity.NOTE_MESSAGE_EXTRA));
         //We need to receive the enum in Note. Not a String
-        Note.Category noteCat = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
-        noteCatButton.setImageResource(Note.categoryToDrawable(noteCat));
+        savedButtonCategory = (Note.Category) intent.getSerializableExtra(MainActivity.NOTE_CATEGORY_EXTRA);
+        noteCatButton.setImageResource(Note.categoryToDrawable(savedButtonCategory));
+        catcat = 0;
+        switch(savedButtonCategory.toString()){
+            case "PERSONAL":
+                catcat = 0;
+                break;
+            case "TECHNICAL":
+                catcat = 1;
+                break;
+            case "QUOTE":
+                catcat = 2;
+                break;
+            case "FINANCE":
+                catcat = 3;
+                break;
+        }
+
 
         // Inflate the layout for this fragment
+        buildCategoryDialog();
+        buildConfigDialog();
+
+        noteCatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                categoryDialogObject.show();
+            }
+        });
+
+        savedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                confirmDialogObject.show();
+            }
+        });
         return fragmentLayout;
+    }
+
+    private void buildCategoryDialog(){
+
+        final String[] categories = new String[]{"Personal","Technical","Quote","Finance"};
+        AlertDialog.Builder categoryBuilder = new AlertDialog.Builder(getActivity());
+        categoryBuilder.setTitle("Choose Note Type");
+
+        categoryBuilder.setSingleChoiceItems(categories, catcat, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                //dismisses our dialog window
+                categoryDialogObject.cancel();
+                switch(item) {
+                    case 0:
+                        savedButtonCategory = Note.Category.PERSONAL;
+                        noteCatButton.setImageResource(R.drawable.p);
+                        break;
+                    case 1:
+                        savedButtonCategory = Note.Category.TECHNICAL;
+                        noteCatButton.setImageResource(R.drawable.t);
+                        break;
+                    case 2:
+                        savedButtonCategory = Note.Category.QUOTE;
+                        noteCatButton.setImageResource(R.drawable.q);
+                        break;
+                    case 3:
+                        savedButtonCategory = Note.Category.FINANCE;
+                        noteCatButton.setImageResource(R.drawable.f);
+                        break;
+                }
+            }
+        });
+        categoryDialogObject = categoryBuilder.create();
+    }
+
+    private void buildConfigDialog(){
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(getActivity());
+        confirmBuilder.setTitle("Are you sure?");
+        confirmBuilder.setMessage("Are you sure you want to save the changes?");
+
+        confirmBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("Save Note", " Note title: " + title.getText() + " Note message: " + message.getText() + " Note category: " + savedButtonCategory);
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        confirmBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //do nothing here
+            }
+        });
+
+        confirmDialogObject = confirmBuilder.create();
+
     }
 
 }
